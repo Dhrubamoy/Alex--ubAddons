5831f1951d09c181a33443233dafbd3bb51bd1fc
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-10-20 14:09:12 +0530

    Update var.py

--- var.py
+++ var.py
@@ -5,5 +5,5 @@ ENV = bool(os.environ.get("ENV", False))
 if ENV:
     from sample_config import Var
 else:
-    if os.path.exists("config.py"):
-        from config import Development as Var
+    if os.path.exists("exampleconfig.py"):
+        from exampleconfig import Development as Var
