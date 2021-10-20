91f1bdb9595de83b8250d4f0c09538c65be4a48d
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-10-20 14:41:28 +0530

    Update Config.py

--- userbot/Config.py
+++ userbot/Config.py
@@ -4,108 +4,23 @@ ENV = bool(os.environ.get("ENV", False))
 if ENV:
     import os
     class Config(object):
-        LOGGER = True
-        # Get this value from my.telegram.org! Please do not steal
-        LOCATION = os.environ.get("LOCATION", None)
-        OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
-        # Get your own ACCESS_KEY from http://api.screenshotlayer.com/api/capture
-        SCREEN_SHOT_LAYER_ACCESS_KEY = os.environ.get("SCREEN_SHOT_LAYER_ACCESS_KEY", None)
-        # Send .get_id in any group to fill this value.
-        SUDO_COMMAND_HAND_LER = os.environ.get("SUDO_COMMAND_HAND_LER", r"\,")
-        BL_CHAT = os.environ.get("BL_CHAT", "-1001344140905")
-        # This is required for the plugins involving the file system.
-        TMP_DOWNLOAD_DIRECTORY = os.environ.get("TMP_DOWNLOAD_DIRECTORY", "./userbot/DOWNLOADS/")
-        # This is required for the speech to text module. Get your USERNAME from https://console.bluemix.net/docs/services/speech-to-text/getting-started.html
-        IBM_WATSON_CRED_URL = os.environ.get("IBM_WATSON_CRED_URL", None)
-        IBM_WATSON_CRED_PASSWORD = os.environ.get("IBM_WATSON_CRED_PASSWORD", None)
-        # This is required for the hash to torrent file functionality to work.
-        HASH_TO_TORRENT_API = os.environ.get("HASH_TO_TORRENT_API", "https://example.com/torrent/{}");
-        # This is required for the @telegraph functionality.
-        TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "LEGENDBOT")
-        # Get a Free API Key from OCR.Space
-        OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
-        # Send .get_id in any group with all your administration bots (added)
-        G_BAN_LOGGER_GROUP = int(os.environ.get("G_BAN_LOGGER_GROUP", -1001169892177))
-        # TG API limit. An album can have atmost 10 media!
-        FBAN_LOGGER_GROUP = os.environ.get("FBAN_LOGGER_GROUP", None)
-        if FBAN_LOGGER_GROUP:
-            FBAN_LOGGER_GROUP = int(FBAN_LOGGER_GROUP)
-
-        GOOGLE_SEARCH_COUNT_LIMIT = int(os.environ.get("GOOGLE_SEARCH_COUNT_LIMIT", 9))
-        TG_GLOBAL_ALBUM_LIMIT = int(os.environ.get("TG_GLOBAL_ALBUM_LIMIT", 9))
-        # MIRROR ACE API KEY AND TOKEN
-        MIRROR_ACE_API_KEY = os.environ.get("MIRROR_ACE_API_KEY", None)
-        MIRROR_ACE_API_TOKEN = os.environ.get("MIRROR_ACE_API_KEY", None)
-        # Telegram BOT Token from @Bot
-        #spootifie
-        SPOTIFY_USERNAME = os.environ.get("SPOTIFY_USERNAME", None)
-        SPOTIFY_PASS = os.environ.get("SPOTIFY_PASS", None)
-        SPOTIFY_BIO_PREFIX = os.environ.get("SPOTIFY_BIO_PREFIX", None)
-        #log
-        DUAL_LOG = os.environ.get("DUAL_LOG", None)
-        # DO NOT EDIT BELOW THIS LINE IF YOU DO NOT KNOW WHAT YOU ARE DOING
-        # TG API limit. A message can have maximum 4096 characters!
-        MAX_MESSAGE_SIZE_LIMIT = 100000
-        # set blacklist_chats where you do not want userbot's features
-        UB_BLACK_LIST_CHAT = set(int(x) for x in os.environ.get("UB_BLACK_LIST_CHAT", "").split())
-        # maximum number of messages for antiflood
-        MAX_ANTI_FLOOD_MESSAGES = 7
-        # warn mode for anti flood
-        ANTI_FLOOD_WARN_MODE = ChatBannedRights(
-            until_date=None,
-            view_messages=None,
-            send_messages=True
-        )
-        # chat ids or usernames, it is recommended to use chat ids,
-        # providing usernames means an additional overhead for the user
-        CHATS_TO_MONITOR_FOR_ANTI_FLOOD = []
-        # Get your own API key from https://www.remove.bg/ or
-        # feel free to use http://telegram.dog/Remove_BGBot
-        REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
-        # Set to True if you want to block users that are spamming your PMs.
-        SLAP_USERNAME = os.environ.get("SLAP_USERNAME", None)
-        GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
-        GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
-        NO_P_M_SPAM = bool(os.environ.get("NO_P_M_SPAM", True))
-        # define "spam" in PMs
-        NO_SONGS = bool(os.environ.get("NO_SONGS", False))
-        MAX_FLOOD_IN_PM = int(os.environ.get("MAX_FLOOD_IN_PM", 7))
-        #pm log
-        PM_LOG_GRP_ID = os.environ.get("PM_LOG_GRP_ID", None)
-        # set to True if you want to log PMs to your PM_LOGGR_BOT_API_ID
-        NC_LOG_P_M_S = bool(os.environ.get("NC_LOG_P_M_S", True))
-        #heroku 
-        HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
-        HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
-        # send .get_id in any channel to forward all your NEW PMs to this group
-        PRIVATE_GROUP_BOT_API_ID = os.environ.get("PRIVATE_GROUP_BOT_API_ID", None)
-        if PRIVATE_GROUP_BOT_API_ID:
-            PRIVATE_GROUP_BOT_API_ID = int(PRIVATE_GROUP_BOT_API_ID)
-        # send .get_id in your private channel to forward all your Private messages
-
-        TAG_LOGGER = os.environ.get("TAG_LOGGER", None)
-        if TAG_LOGGER: TAG_LOGGER = int(TAG_LOGGER)
-
-        #Tag LOGGER
-
-        PM_LOGGR_BOT_API_ID = os.environ.get("PM_LOGGR_BOT_API_ID", None)
-        if PM_LOGGR_BOT_API_ID: PM_LOGGR_BOT_API_ID = int(PM_LOGGR_BOT_API_ID)
-        # For Databases
-        # can be None in which case plugins requiring
-        # DataBase would not work
-        DB_URI = os.environ.get("DATABASE_URL", None)
-        # number of rows of buttons to be displayed in .legend command
-        BUTTONS_IN_HELP = int(os.environ.get("NO_OF_BUTTONS", 7))
-        NO_OF_BUTTONS = int(os.environ.get("NO_OF_BUTTONS", 7))
-        
-        #open load
-        OPEN_LOAD_LOGIN = os.environ.get("OPEN_LOAD_LOGIN", None)
-        OPEN_LOAD_KEY = os.environ.get("OPEN_LOAD_KEY", None)
-        # number of colums of buttons to be displayed in .legend command
         NO_OF_COLOUMS_DISPLAYED_IN_H_ME_CMD = int(os.environ.get("NO_OF_COLUMNS", 2))
         # emoji to be displayed  in help .legend
         NO_OF_COLUMNS = int(os.environ.get("NO_OF_COLUMNS", 2))
         # emoji to be displayed  in help .legend
+        ASSISTANT = os.environ.get("ASSISTANT", None)
+        ABUSE = os.environ.get("ABUSE", None)
+        APP_ID = os.environ.get("APP_ID", None)
+        API_HASH = os.environ.get("API_HASH", None)
+        ALIVE_MSG = os.environ.get("ALIVE_MSG", None)
+        AWAKE_PIC = os.environ.get("AWAKE_PIC", None)
+        ALIVE_PIC = os.environ.get("ALIVE_PIC", None)
+        BL_CHAT = os.environ.get("BL_CHAT", "-1001344140905")
+        G_BAN_LOGGER_GROUP = int(os.environ.get("G_BAN_LOGGER_GROUP", -1001169892177))
+        FBAN_LOGGER_GROUP = os.environ.get("FBAN_LOGGER_GROUP", None)
+        if FBAN_LOGGER_GROUP:
+            FBAN_LOGGER_GROUP = int(FBAN_LOGGER_GROUP)
+
         EMOJI_IN_HELP1 = os.environ.get("EMOJI_IN_HELP1", "⚜️ ")
         EMOJI_IN_HELP2 = os.environ.get("EMOJI_IN_HELP2", "⚡ ")
         ALIVE_EMOJI = os.environ.get("ALIVE_EMOJI", "🔥")
@@ -195,6 +110,92 @@ if ENV:
         FORCE_CHANNEL_ID = int(os.environ.get("FORCE_CHANNEL_ID", False))
         EXTRA_LEGENDBOT = os.environ.get("EXTRA_LEGENDBOT", -1001221881562)
         PM_DATA = os.environ.get("PM_DATA", "ENABLE")
+        GOOGLE_SEARCH_COUNT_LIMIT = int(os.environ.get("GOOGLE_SEARCH_COUNT_LIMIT", 9))
+        # This is required for the speech to text module. Get your USERNAME from https://console.bluemix.net/docs/services/speech-to-text/getting-started.html
+        IBM_WATSON_CRED_URL = os.environ.get("IBM_WATSON_CRED_URL", None)
+        IBM_WATSON_CRED_PASSWORD = os.environ.get("IBM_WATSON_CRED_PASSWORD", None)
+        # This is required for the hash to torrent file functionality to work.
+        HASH_TO_TORRENT_API = os.environ.get("HASH_TO_TORRENT_API", "https://example.com/torrent/{}");
+        LOGGER = True
+        LOCATION = os.environ.get("LOCATION", None)
+        OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
+        OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
+        SCREEN_SHOT_LAYER_ACCESS_KEY = os.environ.get("SCREEN_SHOT_LAYER_ACCESS_KEY", None)
+        # Send .get_id in any group to fill this value.
+        SUDO_COMMAND_HAND_LER = os.environ.get("SUDO_COMMAND_HAND_LER", r"\,")
+        TMP_DOWNLOAD_DIRECTORY = os.environ.get("TMP_DOWNLOAD_DIRECTORY", "./userbot/DOWNLOADS/")
+        TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "LEGENDBOT")
+        TG_GLOBAL_ALBUM_LIMIT = int(os.environ.get("TG_GLOBAL_ALBUM_LIMIT", 9))
+        # MIRROR ACE API KEY AND TOKEN
+        MIRROR_ACE_API_KEY = os.environ.get("MIRROR_ACE_API_KEY", None)
+        MIRROR_ACE_API_TOKEN = os.environ.get("MIRROR_ACE_API_KEY", None)
+        # Telegram BOT Token from @Bot
+        #spootifie
+        SPOTIFY_USERNAME = os.environ.get("SPOTIFY_USERNAME", None)
+        SPOTIFY_PASS = os.environ.get("SPOTIFY_PASS", None)
+        SPOTIFY_BIO_PREFIX = os.environ.get("SPOTIFY_BIO_PREFIX", None)
+        #log
+        DUAL_LOG = os.environ.get("DUAL_LOG", None)
+        # DO NOT EDIT BELOW THIS LINE IF YOU DO NOT KNOW WHAT YOU ARE DOING
+        # TG API limit. A message can have maximum 4096 characters!
+        MAX_MESSAGE_SIZE_LIMIT = 100000
+        # set blacklist_chats where you do not want userbot's features
+        UB_BLACK_LIST_CHAT = set(int(x) for x in os.environ.get("UB_BLACK_LIST_CHAT", "").split())
+        # maximum number of messages for antiflood
+        MAX_ANTI_FLOOD_MESSAGES = 7
+        # warn mode for anti flood
+        ANTI_FLOOD_WARN_MODE = ChatBannedRights(
+            until_date=None,
+            view_messages=None,
+            send_messages=True
+        )
+        # chat ids or usernames, it is recommended to use chat ids,
+        # providing usernames means an additional overhead for the user
+        CHATS_TO_MONITOR_FOR_ANTI_FLOOD = []
+        # Get your own API key from https://www.remove.bg/ or
+        # feel free to use http://telegram.dog/Remove_BGBot
+        REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
+        # Set to True if you want to block users that are spamming your PMs.
+        SLAP_USERNAME = os.environ.get("SLAP_USERNAME", None)
+        GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
+        GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
+        NO_P_M_SPAM = bool(os.environ.get("NO_P_M_SPAM", True))
+        # define "spam" in PMs
+        NO_SONGS = bool(os.environ.get("NO_SONGS", False))
+        MAX_FLOOD_IN_PM = int(os.environ.get("MAX_FLOOD_IN_PM", 7))
+        #pm log
+        PM_LOG_GRP_ID = os.environ.get("PM_LOG_GRP_ID", None)
+        # set to True if you want to log PMs to your PM_LOGGR_BOT_API_ID
+        NC_LOG_P_M_S = bool(os.environ.get("NC_LOG_P_M_S", True))
+        #heroku 
+        HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+        HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
+        # send .get_id in any channel to forward all your NEW PMs to this group
+        PRIVATE_GROUP_BOT_API_ID = os.environ.get("PRIVATE_GROUP_BOT_API_ID", None)
+        if PRIVATE_GROUP_BOT_API_ID:
+            PRIVATE_GROUP_BOT_API_ID = int(PRIVATE_GROUP_BOT_API_ID)
+        # send .get_id in your private channel to forward all your Private messages
+
+        TAG_LOGGER = os.environ.get("TAG_LOGGER", None)
+        if TAG_LOGGER: TAG_LOGGER = int(TAG_LOGGER)
+
+        #Tag LOGGER
+
+        PM_LOGGR_BOT_API_ID = os.environ.get("PM_LOGGR_BOT_API_ID", None)
+        if PM_LOGGR_BOT_API_ID: PM_LOGGR_BOT_API_ID = int(PM_LOGGR_BOT_API_ID)
+        # For Databases
+        # can be None in which case plugins requiring
+        # DataBase would not work
+        DB_URI = os.environ.get("DATABASE_URL", None)
+        # number of rows of buttons to be displayed in .legend command
+        BUTTONS_IN_HELP = int(os.environ.get("NO_OF_BUTTONS", 7))
+        NO_OF_BUTTONS = int(os.environ.get("NO_OF_BUTTONS", 7))
+        
+        #open load
+        OPEN_LOAD_LOGIN = os.environ.get("OPEN_LOAD_LOGIN", None)
+        OPEN_LOAD_KEY = os.environ.get("OPEN_LOAD_KEY", None)
+        # number of colums of buttons to be displayed in .legend command
+
 else:
     class Config(object):
         DB_URI = None
