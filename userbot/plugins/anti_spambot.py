c1eda827df3020131046c0111b7f20c3eae0a904
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-10-22 15:25:37 +0530

    Update anti_spambot.py

--- userbot/plugins/anti_spambot.py
+++ userbot/plugins/anti_spambot.py
@@ -5,7 +5,7 @@ from requests import get
 from telethon.events import ChatAction
 from telethon.tl.types import ChannelParticipantsAdmins, Message
 
-from userbot.Config import (
+from userbot import (
     ANTI_SPAMBOT,
     ANTI_SPAMBOT_SHOUT,
     BOTLOG,
