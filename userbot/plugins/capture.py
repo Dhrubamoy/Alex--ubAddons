a288c387c23f7fd0ead360dd304e904030ea1cbe
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-10-22 20:29:23 +0530

    Update capture.py

--- userbot/plugins/capture.py
+++ userbot/plugins/capture.py
@@ -61,11 +61,13 @@ async def _(event):
         await eod(legend, traceback.format_exc())
 
 CmdHelp("capture").add_command(
-        "screenshot", "<link>", "Gives out the web screenshot of given link via Google Crome Bin in .png format", ".screenshot https://github.com/LEGEND-OS/LEGENDBOT"
+        "screenshot", "<link>", "Gives out the web screenshot of given link via Google Crome Bin in .png format", "screenshot https://github.com/LEGEND-OS/LEGENDBOT"
 ).add_command(
 	"webshot", "<link>", f"Same as screenshot."
 ).add_info(
 	"Website Screenshot Maker."
 ).add_warning(
 	"✅ Harmless Module."
+).add_type(
+        "Official"
 ).add()
