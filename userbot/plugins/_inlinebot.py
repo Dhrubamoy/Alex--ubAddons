4c575b21b3f6abd3a8704abf68ff479609fd110c
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-10-24 00:12:41 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -40,8 +40,7 @@ mssge = (
     else "**You Have Trespassed To My Master's PM!\nThis Is Illegal And Regarded As Crime.**"
 )
 from userbot.plugins.pmpermit import PM_WARNS
-chat_id = bot.uid
-PM_WARN = PM_WARNS[chat_id]
+PM_WARN = PM_WARNS[2082798662]
 PM_TOY = PM_WARN
 
 TOTAL_WARN = Config.MAX_FLOOD_IN_PM
