2201c3999151fe9616cce1dfe6a3ea971022c4e0
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-10-24 20:25:13 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -115,7 +115,7 @@ def button(page, modules):
 if Config.BOT_USERNAME is not None and tgbot is not None:
     @tgbot.on(InlineQuery)  # pylint:disable=E0602
     async def inline_handler(event):
-        PM_TOY = {PM_WARNS[event.chat_id]}
+        PM_TOY = PM_WARNS
         builder = event.builder
         result = None
         query = event.text
