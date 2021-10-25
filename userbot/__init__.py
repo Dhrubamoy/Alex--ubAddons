017ca1b9b8436ceb5dbe27f0b61deb9c998f804f
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-10-25 14:40:26 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -44,6 +44,24 @@ LegendBot = TelegramClient(
 bot = kbot = Legend
 tbot = LegendBot
 
+REDIS_KEY = os.environ.get("REDIS_KEY", None)
+REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", None)
+if REDIS_KEY and REDIS_PASSWORD:
+    import redis
+    redis_info = REDIS_KEY.split(":")
+    dB = redis.StrictRedis(
+        host=redis_info[0],
+        port=redis_info[1],
+        password=REDIS_PASSWORD,
+        charset="utf-8",
+        decode_responses=True,
+    )
+    print("Connected To Redis")
+else:
+    print("Could Not Connnect To Redis Check Your Redis Key And Pass if facing issue come to support group @Legend_userbot Quitting!")
+    sys.exit(1)
+
+udb = db
 
 DEVS = ["2082798662"]
 CMD_LIST = {}
