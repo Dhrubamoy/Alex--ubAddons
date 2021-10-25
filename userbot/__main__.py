e90695be435ff98b1295533f1864bf29c92917ec
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-10-25 15:26:30 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -19,9 +19,23 @@ l2= Config.SUDO_COMMAND_HAND_LER
 LEGEND_PIC = "https://te.legra.ph/file/a3e358b1331d6ef9a6299.mp4"
 l1 = Config.COMMAND_HAND_LER
 
+REDIS_KEY = os.environ.get("REDIS_KEY", None)
+REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", None)
+if REDIS_KEY and REDIS_PASSWORD:
+    import redis
+    redis_info = REDIS_KEY.split(":")
+    dB = redis.StrictRedis(
+        host=redis_info[0],
+        port=redis_info[1],
+        password=REDIS_PASSWORD,
+        charset="utf-8",
+        decode_responses=True,
+    )
+    print("Connected To Redis")
+else:
+    print("Could Not Connnect To Redis Check Your Redis Key And Pass if facing issue come to support group @Legend_userbot Quitting!")
 
-LOAD_USERBOT = os.environ.get("LOAD_USERBOT", True)
-LOAD_ASSISTANT = os.environ.get("LOAD_ASSISTANT", True)    
+udb = dB
 
 async def add_bot(bot_token):
     try:
