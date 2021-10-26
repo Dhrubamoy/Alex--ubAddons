628730d229e3ccc8db78a19b24a1c564cba1ff19
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-10-26 11:07:02 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -160,12 +160,18 @@ async def _(event):
                 sixteen = await conv.get_response()
                 seventeen = await conv.send_message(description)
                 eightteen = await conv.get_response()
+                19 = await conv.send_message("/setuserpic")
+                20 = await conv.get_response()
+                21 = await bot.send_file(botname)
+                22 = await conv.get_response()
+                23 = await conv.send_message("https://te.legra.ph/file/0a1fa23cbb1e6520f6550.jpg")
+                24 = await conv.get_response()
                 await bot.send_read_acknowledge(conv.chat_id)
             except YouBlockedUserError:
                 return await legend.edit("Unblock @Botfather first.")
             await event.edit(f"**Turned On Assistance Successfully.\nClick Here 👉{botname} & Add To Any Group**")
             await bot.delete_messages(
-                conv.chat_id, [first.id, second.id, third.id, fourth.id, fifth.id, sixth.id, seventh.id, eighth.id, nineth.id, tenth.id, eleventh.id, twelveth.id, thirdteen.id, fourteen.id, fiveteen.id, sixteen.id, seventeen.id, eightteen.id]
+                conv.chat_id, [first.id, second.id, third.id, fourth.id, fifth.id, sixth.id, seventh.id, eighth.id, nineth.id, tenth.id, eleventh.id, twelveth.id, thirdteen.id, fourteen.id, fiveteen.id, sixteen.id, seventeen.id, eightteen.id, 19.id, 20.id, 21.id, 22.id, 23.id, 24.id]
                 )
     else:
         return await event.edit("**Plz First Turn On Assistant.** Click Here👉 `.set var ASSISTANT ON`")
